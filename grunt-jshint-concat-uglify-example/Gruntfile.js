5fef6f1a25986f6ec1213e806681f9416e3088af
Anastasios Zouzias <zouzias@gmail.com>
2015-07-14 19:55:58 +0200

    Add clean task and comments

--- grunt-jshint-concat-uglify-example/Gruntfile.js
+++ grunt-jshint-concat-uglify-example/Gruntfile.js
@@ -1,44 +1,49 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-		uglify: {
+		uglify: { /* Minifies JS code */
 			options: {
 				mangle: true,
 				compress: true,
-				sourceMap: "dist/application.map",
-				banner: "/* Anastasios Zouzias 2015*/\n"
+				sourceMap: 'dist/application.map',
+				banner: '/* Anastasios Zouzias 2015*/\n'
 			},
 			target: {
-				src: "dest/application.js",
-				dest: "dist/application.min.js"
+				src: 'dest/application.js',
+				dest: 'dist/application.min.js'
 			}
 		},
-		jshint: {
+		jshint: { /* Checks Javascript for error */
 			options : {
 				eqeqeq : true, /* Only ===, not ==*/
 				curly: true,  /* Use curly everywhere*/
 				undef: true
 			},
 			target: {
-				src : "src/*.js"
+				src : 'src/*.js'
 			}
 		},
-		concat: {
+		concat: { /* Concats the following files to dest/application.js */
 			options: {
-				seperator: ";"
+				seperator: ';'
 			},
 			target: {
-				src: ["src/application.js", "src/util.js"],
-				dest: "dest/application.js"
+				src: ['src/application.js', 'src/util.js'],
+				dest: 'dest/application.js'
 			}
+		},
+		clean: { /* Clean dest and dist directories*/
+			target: ['dist', 'dest']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	/* Run tasks : jshint, concat and uglify*/
-	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+	grunt.registerTask('rebuild', ['clean', 'default']);
 
 }
